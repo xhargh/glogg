6caee58a233ec3968ee523059de076a7a2fd081a
Gustav Andersson <xhargh@gmail.com>
2020-06-28 17:21:39 +0200

    Updated path

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -571,7 +571,7 @@ void MainWindow::about()
 #ifdef GLOGG_COMMIT
                 "<p>Built " GLOGG_DATE " from " GLOGG_COMMIT
 #endif
-               "<p><a href=\"http://github.com/xhargh/glogg/\">http://github.com/xhargh/glogg/</a></p>"
+               "<p><a href=\"http://github.com/xhargh/glogg-io/\">http://github.com/xhargh/glogg-io/</a></p>"
                "<p><a href=\"http://glogg.bonnefon.org/\">http://glogg.bonnefon.org/</a></p>"
                "<p>Copyright &copy; 2020 Gustav Andersson"
                "<p>Copyright &copy; 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 Nicolas Bonnefon and other contributors"

8053d5a5f04801025a9bbe0a4a737577fdc814c7
Gustav Andersson <xhargh@gmail.com>
2020-06-30 21:58:09 +0200

    Started to port the serial port log data class

--- src/logdata/include/data/serialportlogdata.h
+++ src/logdata/include/data/serialportlogdata.h
@@ -3,10 +3,59 @@
 
 #include "iodevicelogdata.h"
 
+#include <QObject>
+#include <QSerialPort>
+
+#include "serialportsettings.h"
+
+class QSerialPort;
+
 class SerialPortLogData : public IoDeviceLogData {
     Q_OBJECT
-public:
-    SerialPortLogData();
+  private:
+    std::vector<std::pair<QDateTime, QString>> m_lines;
+    QSerialPort m_serialPort;
+    SerialPortSettings m_serialPortSettings;
+    int m_maxLineLength;
+  public:
+    // Creates an empty LogData
+    SerialPortLogData(SerialPortSettings* settings);
+    // Destroy an object
+    ~SerialPortLogData();
+
+    virtual void attachFile( const QString& fileName ) override;
+    // qqq virtual void write(QString str) override;
+    // qqq virtual bool isWritable() const override;
+    // qqq virtual SerialPortSettings * GetIoSettings() override;
+    virtual void reload(QTextCodec* forcedEncoding = nullptr) override;
+
+    // AbstractLogData interface
+  protected:
+    virtual QString doGetLineString(LineNumber line) const override;
+    virtual QString doGetExpandedLineString(LineNumber line) const override;
+    virtual std::vector<QString> doGetLines(LineNumber first_line, LinesCount number) const override;
+    virtual std::vector<QString> doGetExpandedLines(LineNumber first_line, LinesCount number) const override;
+    // Get the length of the longest line
+    virtual LinesCount doGetNbLine() const override;
+    // Get the total number of lines
+    virtual LineLength doGetMaxLength() const override;
+    virtual LineLength doGetLineLength(LineNumber line) const override;
+    virtual void doSetDisplayEncoding( const char* encoding) override;
+
+    virtual QDateTime getLastModifiedDate() const override;
+
+  signals:
+    // Sent during the 'attach' process to signal progress
+    // percent being the percentage of completion.
+    void loadingProgressed( int percent );
+    // Signal the client the file is fully loaded and available.
+    void loadingFinished( LoadingStatus status );
+    // Sent when the file on disk has changed, will be followed
+    // by loadingProgressed if needed and then a loadingFinished.
+    void fileChanged( MonitoredFileStatus status );
+
+  private slots:
+    void readDataSlot();
 };
 
 #endif // SERIALPORTLOGDATA_H
--- src/logdata/src/serialportlogdata.cpp
+++ src/logdata/src/serialportlogdata.cpp
@@ -1,5 +1,133 @@
 #include "serialportlogdata.h"
 
-SerialPortLogData::SerialPortLogData() : IoDeviceLogData() {
-    qInfo() << __func__;
+
+SerialPortLogData::SerialPortLogData(SerialPortSettings *settings) : IoDeviceLogData()
+{
+    m_serialPort.setPortName(settings->name);
+    m_serialPort.setBaudRate(settings->baudRate);
+    m_serialPort.setDataBits(settings->dataBits);
+    m_serialPort.setParity(settings->parity);
+    m_serialPort.setStopBits(settings->stopBits);
+    m_serialPort.setFlowControl(settings->flowControl);
+}
+
+SerialPortLogData::~SerialPortLogData()
+{
+
+}
+
+void SerialPortLogData::attachFile(const QString &fileName)
+{
+    m_serialPort.setPortName(fileName);
+    if (m_serialPort.open(QIODevice::ReadWrite)) {
+        qInfo() << __func__ << " " << fileName << " opened successfully";
+        connect(&m_serialPort, &QSerialPort::readyRead, this, &SerialPortLogData::readDataSlot);
+    }
+}
+
+/* qqq
+void SerialLogData::write(QString str)
+{
+    qInfo() << "write: " + str;
+    m_serialPort.write(str.toLatin1() + "\n");
+}
+
+bool SerialLogData::isWritable() const
+{
+    return true;
+}
+
+SerialPortSettings *SerialLogData::GetIoSettings()
+{
+    return &m_serialPortSettings;
+}
+*/
+
+
+void SerialPortLogData::reload(QTextCodec *forcedEncoding)
+{
+    (void)forcedEncoding;
+    m_lines.clear();
+    emit fileChanged( MonitoredFileStatus::Truncated );
+    emit loadingFinished ( LoadingStatus::Successful );
+}
+
+QString SerialPortLogData::doGetLineString(LineNumber line) const
+{
+    if (m_lines.empty()) {
+        return QString();
+    }
+    return m_lines[line.get()].second;
+}
+
+QString SerialPortLogData::doGetExpandedLineString(LineNumber line) const
+{
+    if (m_lines.empty()) {
+        return QString();
+    }
+    auto l = m_lines[line.get()];
+    return l.first.toString(Qt::ISODateWithMs) + " " + l.second;
+}
+
+std::vector<QString> SerialPortLogData::doGetLines(LineNumber first_line, LinesCount number) const
+{
+    std::vector<QString> qs;
+    for (unsigned int i = 0; i < number.get(); i++) {
+        qs.push_back(doGetLineString(LineNumber(i + first_line.get())));
+    }
+    return qs;
+}
+
+std::vector<QString> SerialPortLogData::doGetExpandedLines(LineNumber first_line, LinesCount number) const
+{
+    std::vector<QString> qs;
+    for (unsigned int i = 0; i < number.get(); i++) {
+        qs.push_back(doGetExpandedLineString(LineNumber(i + first_line.get())));
+    }
+    return qs;
+}
+
+LinesCount SerialPortLogData::doGetNbLine() const
+{
+    return LinesCount(static_cast<unsigned int>(m_lines.size()));
+}
+
+LineLength SerialPortLogData::doGetMaxLength() const
+{
+    return LineLength(m_maxLineLength);
+}
+
+LineLength SerialPortLogData::doGetLineLength(LineNumber line) const
+{
+    return LineLength(doGetLineString(line).length());
+}
+
+void SerialPortLogData::doSetDisplayEncoding(const char *encoding)
+{
+    qInfo() << __func__ << encoding << "\n";
+}
+
+QDateTime SerialPortLogData::getLastModifiedDate() const
+{
+    if (m_lines.size() == 0) {
+        return QDateTime::currentDateTime();
+    } else {
+        return m_lines.back().first;
+    }
+}
+
+void SerialPortLogData::readDataSlot()
+{
+    while (m_serialPort.canReadLine()) {
+        const QByteArray data = m_serialPort.readLine();
+        QString d = QString(data);
+        d.remove(QRegExp("[\\n\\r]")); // remove new line and carrage return
+        if (d.length() > m_maxLineLength) {
+            m_maxLineLength = d.length();
+        }
+        m_lines.push_back(std::make_pair(QDateTime::currentDateTime(), d));
+        qDebug() << d;
+        emit fileChanged( MonitoredFileStatus::DataAdded );
+        emit loadingFinished ( LoadingStatus::Successful );
+    }
 }

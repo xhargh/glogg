c589bc635ec866442d6f17c2c96f0beabe739472
Gustav Andersson <xhargh@gmail.com>
2020-08-14 12:20:26 +0200

    Trying to fix dmg creation

--- scripts/osx/prepare_bundle.sh
+++ scripts/osx/prepare_bundle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-macdeployqt ./output/klogg.app -always-overwrite -verbose=2
-python ../3rdparty/macdeployqtfix/macdeployqtfix.py ./output/klogg.app/Contents/MacOS/klogg $(brew --prefix qt5)
+macdeployqt ./output/klogg-io.app -always-overwrite -verbose=2
+python ../3rdparty/macdeployqtfix/macdeployqtfix.py ./output/klogg-io.app/Contents/MacOS/klogg-io $(brew --prefix qt5)
